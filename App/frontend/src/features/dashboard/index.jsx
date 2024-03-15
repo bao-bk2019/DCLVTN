ff36c4ea7d5b905ee74ee2fd369d8a74b2adc093
bao-bk2019 <duongquanbao19962012@gmail.com>
2024-03-15 11:54:25 +0700

    new update

---
+++ App/frontend/src/features/dashboard/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import TableFeature from '../table';
+import { Button } from 'react-bootstrap';
+import { Link, Outlet } from 'react-router-dom';
+import './styles.scss'
+function AppHeader() {
+    return (
+        <>
+            <div className='mx-auto'>
+                <TableFeature />
+            </div>
+            <div className='center'>
+                <Button variant="warming">
+                    <Link to="action1" >Cluster</Link>
+                </Button>{' '}
+                <Button variant="warming">
+                    <Link to="action2">LSTM</Link>
+                </Button>{' '}
+                <Button variant="warming">
+                    <Link to="chart" >Chart</Link>
+                </Button>{' '}
+
+            </div>
+            <Outlet />
+        </>
+    );
+};
+
+export default AppHeader;
