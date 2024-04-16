6cc734a2a0096d275f7945160c2cb9147c61560f
huynhat0207 <huynhat0207@gmail.com>
2024-04-16 15:39:49 +0700

    update outlet

--- App/frontend/src/App.js
+++ App/frontend/src/App.js
@@ -17,7 +17,7 @@ import Notification from './features/user/notification/index.jsx'
 import Edit from './features/user/edit/index.jsx'
 import Tutorial from './features/tutorial/index.jsx';
 import Security from './features/user/security/index.jsx';
-import getStarted from './features/tutorial/getstarted/first/index.jsx';
+import GetStarted from './features/tutorial/getstarted/first/index.jsx';
 
 import { AuthProvider } from "./AuthContext.js";
 
